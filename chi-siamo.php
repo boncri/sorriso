c40eafb979193d71d8292d9d4640a3dc90a7b88f
Cristiano Boncompagni <cristiano.boncompagni@intersail.it>
2017-12-19 23:15:36 +0100

    Chi Siamo PHP page

---
+++ chi-siamo.php
@@ -0,0 +1,84 @@
+<?php
+    $status = [
+        'cur_page' => 'Chi Siamo'
+    ];
+?>
+
+<?php require_once('_global.php') ?>
+    
+<!DOCTYPE html>
+<html lang="it">
+    <?php require_once('_head.php'); ?>            
+  
+    <body>
+
+<!--gt Wrapper Start-->  
+<div class="gt_wrapper">
+
+    <!--Header Wrap Start-->
+    <?php require_once('_body_header.php'); ?>
+    <!--Header Wrap End-->
+  
+    <!--Sub Banner Wrap Start -->
+    <?php require_once('_sub_banner.php'); ?>
+    <!--Sub Banner Wrap End -->
+
+    <!--Main Content Wrap Start-->
+    <div class="gt_main_content_wrap">
+        <!--About Us Wrap Start-->
+        <section class="gt_about_bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="gt_about_wrap">
+                            <h4 class="title">Associazione <i>"Costruiamo il futuro"</i></h4>
+                            <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi. odio. Sed non  mauris vitae erat consequat auctor eu in elit. </p>
+                            <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauri vulputate cursus a sit amet mauris.  </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="gt_about_img">
+                            <img src="extra-images/about_us_img.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--About Us Wrap End-->
+        
+      <!--Footer Wrap Start-->
+      <?php require_once('_footer.php') ?>
+      <!--Footer Wrap End-->
+    
+    <!--Back to Top Wrap Start-->
+    <?php require_once('_back_to_top.php') ?>
+    <!--Back to Top Wrap Start-->
+
+</div>
+<!--gt Wrapper End-->
+
+
+
+    <!--Jquery Library-->
+    <script src="js/jquery.js"></script>
+    <!--Bootstrap core JavaScript-->
+    <script src="js/bootstrap.min.js"></script>
+    <!--Accordian JavaScript-->
+    <script src="js/jquery.accordion.js"></script>
+    <!--Count Down JavaScript-->
+    <script src="js/jquery.downCount.js"></script>
+    <!--Pretty Photo JavaScript-->
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <!--Owl Carousel JavaScript-->
+    <script src="js/owl.carousel.js"></script>
+    <!--Number Count (Waypoint) JavaScript-->
+    <script src="js/waypoints-min.js"></script>
+    <!--Filter able JavaScript-->
+    <script src="js/jquery-filterable.js"></script>
+    <!--WOW JavaScript-->
+    <script src="js/wow.min.js"></script>
+    <!--Custom JavaScript-->
+    <script src="js/custom.js"></script>
+
+  </body>
+</html>
