b7a809265ba2bb8312012384014bca6f28f0e1ff
Cristiano Boncompagni <cristiano.boncompagni@intersail.it>
2017-12-19 23:28:58 +0100

    Adds 404 PHP page

---
+++ 404.php
@@ -0,0 +1,88 @@
+<?php
+    $status = [
+        "cur_page": "Home"
+    ]
+?>
+
+<?php require_once('_global.php') ?>
+
+<!DOCTYPE html>
+<html lang="en">
+        <?php require_once('_head.php') ?>
+
+  <body>
+
+<!--gt Wrapper Start-->  
+<div class="gt_wrapper page_404_margin">
+
+    <!--Header Wrap Start-->
+    <?php require_once('_body_header.php') ?>
+    <!--Header Wrap End-->
+
+    <!--Sub Banner Wrap Start -->
+    <?php require_once('_sub_banner.php') ?>
+    <!--Sub Banner Wrap End -->
+
+    <!--404 Wrap Start-->
+    <div class="container">
+        <div class="gt_404_outer_wrap default_width">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="gt_404_text_outer_wrap default_width">
+                        <div class="gt_404_text default_width">
+                            <h2>4</h2>
+                            <img src="images/404-face.png" alt="">
+                            <h2>4</h2>
+                        </div>
+                        <div class="gt_404_text">
+                            <p>Oops... Pagina non trovata</p>
+                            <a href="index.html">Torna all'indice</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="gt_404_img default_width">
+                        <img src="images/404-kid.png" alt="">
+                    </div>
+                </div>
+            </div>  
+        </div>
+    </div>
+    <!--404 Wrap End-->
+
+    <!--Footer Wrap Start-->
+    <?php require_once('_footer.php') ?>
+    <!--Footer Wrap End-->
+
+    <!--Back to Top Wrap Start-->
+    <?php require_once('_back_to_top.php') ?>
+    <!--Back to Top Wrap Start-->
+
+</div>
+<!--gt Wrapper End-->
+
+
+
+    <!--Jquery Library-->
+    <script src="js/jquery.js"></script>
+    <!--Bootstrap core JavaScript-->
+    <script src="js/bootstrap.min.js"></script>
+    <!--Accordian JavaScript-->
+    <script src="js/jquery.accordion.js"></script>
+    <!--Count Down JavaScript-->
+    <script src="js/jquery.downCount.js"></script>
+    <!--Pretty Photo JavaScript-->
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <!--Owl Carousel JavaScript-->
+    <script src="js/owl.carousel.js"></script>
+    <!--Number Count (Waypoint) JavaScript-->
+    <script src="js/waypoints-min.js"></script>
+    <!--Filter able JavaScript-->
+    <script src="js/jquery-filterable.js"></script>
+    <!--WOW JavaScript-->
+    <script src="js/wow.min.js"></script>
+    <!--Custom JavaScript-->
+    <script src="js/custom.js"></script>
+
+  </body>
+</html>
